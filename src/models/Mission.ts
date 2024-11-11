e3b6b12168ee368e11186cba13a656d735d47a6b
kairayary <kaira.abreu@gmail.com>
2024-11-10 23:04:57 -0300

    Se agregó el atributo isCompleted, mejora en el método para completar la misión y se añadió el método para reiniciar la misión

--- src/models/Mission.ts
+++ src/models/Mission.ts
@@ -1,44 +1,54 @@
 export enum MissionType {
     Main = "Main", //Mision principal
     Side = "Side", //Mision secundaria
-    Event= "Event", // Mision de evento
-  }
+    Event = "Event", // Mision de evento
+}
 
 export class Mission {
     private _description: string;
     private _difficulty: number;
     private _reward: number;
     private _type: MissionType;
-    
+    private _isCompleted: boolean = false; // Para saber si la misión está completada
+
 
     constructor(description: string, difficulty: number, reward: number, type: MissionType) {
         this._description = description;
         this._difficulty = difficulty;
         this._reward = reward;
         this._type = type;
-        
-    }
+
+    };
+
     public get description(): string {
         return this._description;
-    }
+    };
+
     public get difficulty(): number {
         return this._difficulty;
-    }
+    };
+
     public get reward(): number {
         return this._reward;
-    }
+    };
+
     public get type(): MissionType {
         return this._type;
-    }
+    };
+
+    public get isCompleted(): boolean {
+        return this._isCompleted;
+    };
 
     //Metodos set para mofificar los valores 
+
     public set description(description: string) {
         if (description && description.trim().length > 0) {
             this._description = description;
         } else {
             console.log("La descripción no puede estar vacía.");
         }
-    }
+    };
 
     public set difficulty(difficulty: number) {
         if (difficulty >= 1 && difficulty <= 10) {
@@ -46,7 +56,7 @@ export class Mission {
         } else {
             console.log("La dificultad debe estar entre 1 y 10.");
         }
-    }
+    };
 
     public set reward(reward: number) {
         if (reward >= 0) {
@@ -54,17 +64,34 @@ export class Mission {
         } else {
             console.log("La recompensa no puede ser negativa.");
         }
-    }
+    };
 
     public set type(type: MissionType) {
         this._type = type;
     }
-     
+
     // Método para completar la misión
-     public complete(): void {
-        console.log(`Misión completada: ${this._description}`);
-        console.log(`Tipo de misión: ${this._type}`);
-        console.log(`Dificultad: ${this._difficulty}`);
-        console.log(`Recompensa obtenida: ${this._reward} puntos de experiencia.`);
-    }
-}
+
+    public complete(): void {
+
+        if (!this._isCompleted) {
+            //Actualizamos el valor a True
+            this._isCompleted = true;
+
+            console.log(`Misión completada: ${this._description}`);
+            console.log(`Tipo de misión: ${this._type}`);
+            console.log(`Dificultad: ${this._difficulty}`);
+            console.log(`Recompensa obtenida: ${this._reward} puntos de experiencia.`);
+
+        } else {
+            console.log("La misión ya ha sido completada.");
+        };
+
+    };
+
+    // Método para reiniciar la misión
+    public resetMission(): void {
+        this._isCompleted = false;
+        console.log(`La misión "${this._description}" ha sido reiniciada.`);
+    };
+};
